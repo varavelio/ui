4b0f3356fcd9b6748fa35eb019a2409c44034e06
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-14 04:29:05 +0000

    feat: integrate theme handling in ToastProvider for enhanced visual consistency

--- src/lib/runtime/toast/ToastProvider.svelte
+++ src/lib/runtime/toast/ToastProvider.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { Toaster } from "svelte-sonner";
+  import { theme } from "../theme/index.js";
 </script>
 
 <!-- https://github.com/wobsoriano/svelte-sonner (MIT license) -->
-<Toaster position="bottom-right" expand={true} richColors />
+<Toaster position="bottom-right" expand={true} theme={theme.resolved} richColors />
