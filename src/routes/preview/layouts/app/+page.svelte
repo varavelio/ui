b4db8c3a4bffa42a7406fe8037531c6e7a49db58
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-19 23:25:53 +0000

    add app layout demo

--- src/routes/preview/layouts/app/+page.svelte
+++ src/routes/preview/layouts/app/+page.svelte
@@ -1,14 +1,37 @@
 <script lang="ts">
-  import { Container } from "$lib/components/index.js";
+  import { House } from "@lucide/svelte";
+  import Heading from "$lib/components/Heading/Heading.svelte";
+  import { Breadcrumbs, ThemePicker } from "$lib/components/index.js";
   import { AppLayout } from "$lib/layouts/index.js";
 </script>
 
-<Container size="xl" class="mx-auto overflow-hidden">
-  <AppLayout.Root>
-    <AppLayout.Header>Header</AppLayout.Header>
-    <div class="flex h-full">
-      <AppLayout.Sidebar>Sidebar</AppLayout.Sidebar>
-      <AppLayout.Main>Main Content</AppLayout.Main>
+<AppLayout primaryRegion="sidebar" maxWidth="lg">
+  {#snippet header()}
+    <div class="w-full flex justify-between items-center">
+      <Heading size="md" level="2">Header</Heading>
+      <ThemePicker class="-mr-2" /> 
     </div>
-  </AppLayout.Root>
-</Container>
+  {/snippet}
+
+  {#snippet sidebar()}
+    <nav class="sticky top-4 desk:top-0">
+      <Heading size="md" level="2">Sidebar</Heading>
+    </nav>
+    <div class="h-[200dvh]"></div>
+  {/snippet}
+
+  {#snippet main()}
+    <div class="sticky top-0 p-4 bg-base-100">
+      <Breadcrumbs
+        items={[
+          { icon: House, label: "Home", href: "#" },
+          { label: "Demo", href: "#" },
+        ]}
+      />
+    </div>
+    <div class="p-4 pt-0">
+      <Heading size="2xl">Main content</Heading>
+      <div class="h-[200dvh]"></div>
+    </div>
+  {/snippet}
+</AppLayout>
