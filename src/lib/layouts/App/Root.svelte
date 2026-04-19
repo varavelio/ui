3cba95fde7da61c6b590f4fc8610952005b9ca9a
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-19 05:44:45 +0000

    feat: implement Root component with initial layout and state management

---
+++ src/lib/layouts/App/Root.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { AppLayoutState, setAppLayoutState } from "./state.svelte.js";
+
+  interface Props {
+    children: Snippet;
+  }
+
+  let { children }: Props = $props();
+
+  setAppLayoutState(new AppLayoutState());
+</script>
+
+<div class="w-dvw h-dvh overflow-hidden">{@render children()}</div>
