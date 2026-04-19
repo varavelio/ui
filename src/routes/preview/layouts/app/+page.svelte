9115f33a5947dfb119e79c5bc1d2549e78fd23eb
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-19 05:56:24 +0000

    feat: add app layout structure with Container, Header, Sidebar, and Main components

---
+++ src/routes/preview/layouts/app/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Container } from "$lib/components/index.js";
+  import { AppLayout } from "$lib/layouts/index.js";
+</script>
+
+<Container size="xl" class="mx-auto overflow-hidden">
+  <AppLayout.Root>
+    <AppLayout.Header>Header</AppLayout.Header>
+    <div class="flex h-full">
+      <AppLayout.Sidebar>Sidebar</AppLayout.Sidebar>
+      <AppLayout.Main>Main Content</AppLayout.Main>
+    </div>
+  </AppLayout.Root>
+</Container>
