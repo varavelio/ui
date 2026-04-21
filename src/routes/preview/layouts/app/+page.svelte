95c49a9ad52ad9ed3714ead3d4a8728812519750
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-21 16:21:22 +0000

    feat: refactor layout snippets in +page.svelte for improved structure and organization

--- src/routes/preview/layouts/app/+page.svelte
+++ src/routes/preview/layouts/app/+page.svelte
@@ -1,31 +1,55 @@
 <script lang="ts">
-  import { House } from "@lucide/svelte";
+  import { House, LayoutDashboard, Settings } from "@lucide/svelte";
   import Heading from "$lib/components/Heading/Heading.svelte";
-  import { Breadcrumbs, ThemePicker } from "$lib/components/index.js";
+  import {
+    Breadcrumbs,
+    Button,
+    Input,
+    Nav,
+    ThemePicker,
+  } from "$lib/components/index.js";
   import { AppLayout } from "$lib/layouts/index.js";
 </script>
 
 <AppLayout primaryRegion="sidebar" maxWidth="lg">
-  {#snippet header()}
-    <div class="w-full flex justify-between items-center">
-      <Heading size="md" level="2">Header</Heading>
-      <ThemePicker class="-mr-2" />
-    </div>
+  {#snippet headerLeft()}
+    <Heading size="md" level="2">Header</Heading>
   {/snippet}
 
-  {#snippet sidebar()}
+  {#snippet headerCenter()}
+    <Input placeholder="Search..." class="hidden desk:block" />
+  {/snippet}
+
+  {#snippet headerRight()}
+    <ThemePicker class="-mr-2 hidden desk:block" />
+  {/snippet}
+
+  {#snippet sidebarTop()}
     <nav class="sticky top-4 desk:top-0">
       <Heading size="md" level="2">Sidebar</Heading>
     </nav>
+  {/snippet}
+
+  {#snippet sidebarCenter()}
+    <Nav.Root>
+      <Nav.Group label="General" icon={LayoutDashboard} open>
+        <Nav.Item href="#" icon={House} label="Home" active />
+        <Nav.Item href="#" icon={Settings} label="Settings" />
+      </Nav.Group>
+    </Nav.Root>
     <div class="h-[200dvh]"></div>
+  {/snippet}
+
+  {#snippet sidebarBottom()}
+    <Button>Click me</Button>
   {/snippet}
 
   {#snippet main()}
     <div class="sticky top-0 p-4 bg-base-100">
       <Breadcrumbs
         items={[
-          { icon: House, label: "Home", href: "#" },
-          { label: "Demo", href: "#" },
+          { label: "Dashboard", href: "#", icon: LayoutDashboard },
+          { label: "Home", href: "#" },
         ]}
       />
     </div>
