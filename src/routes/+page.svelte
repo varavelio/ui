3dc17227382aa5824b1c6251e64928dcec9afd6b
Luis Eduardo <eduardo.devop@gmail.com>
2026-04-21 05:14:50 +0000

    feat: implement redirection to /components/avatar in +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
+
+  if (browser) {
+    goto("/components/avatar", { replaceState: true });
+  }
+</script>
+
+<svelte:head>
+  <meta http-equiv="refresh" content="0;url=/components/avatar">
+</svelte:head>
